9d11eba5867dba57f4991d09a16b5fa75a742c19
darklorian <lolpokens@mail.ru>
2024-02-07 13:54:20 +0400

    Add place to models

--- containers/backend/app/deployment/migrations/env.py
+++ containers/backend/app/deployment/migrations/env.py
@@ -7,6 +7,8 @@ from sqlalchemy import pool
 from alembic import context
 from sqlmodel import SQLModel  # noqa
 
+# models import:
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
