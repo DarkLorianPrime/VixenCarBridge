0c7a3f49631104254c07c1fe9d35151514dec212
darklorian <lolpokens@mail.ru>
2024-02-09 15:53:22 +0400

    Add base to models with the possibility of soft removal

--- containers/backend/app/storages/database/models/base.py
+++ containers/backend/app/storages/database/models/base.py
@@ -7,3 +7,8 @@ from sqlmodel import SQLModel, Field
 class Base(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, unique=True, nullable=False, index=True)
     created_at: datetime = Field(default_factory=datetime.now)
+
+
+class DeletingBase(Base):
+    removed: bool = Field(default=False)
+    removed_at: datetime = Field(default=None)
