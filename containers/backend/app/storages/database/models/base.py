4c22fa7388e3ab4b2b14f6e9be84b3bdd00151b0
darklorian <lolpokens@mail.ru>
2024-02-09 15:48:20 +0400

    Create base to all models

---
+++ containers/backend/app/storages/database/models/base.py
@@ -0,0 +1,9 @@
+import uuid
+from datetime import datetime
+
+from sqlmodel import SQLModel, Field
+
+
+class Base(SQLModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, unique=True, nullable=False, index=True)
+    created_at: datetime = Field(default_factory=datetime.now)
