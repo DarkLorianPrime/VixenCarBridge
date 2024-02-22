91dd065de5a6eeb13cb2dc14a2b66d45a076e18b
darklorian <lolpokens@mail.ru>
2024-02-22 23:37:35 +0400

    add env to test logger

--- containers/backend/app/main.py
+++ containers/backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from starlette.requests import Request
 
 app = FastAPI(
     title="VixenCarBridge",
@@ -7,8 +8,9 @@ app = FastAPI(
 main_router = APIRouter(prefix="/api/v1", tags=["Vixen CarBridge"])
 
 
-@main_router.get("/hello")
-async def pong():
-    return "world"
+@main_router.get("/status")
+async def pong(request: Request):
+    print(request.headers)
+    return "ok"
 
 app.include_router(main_router)
