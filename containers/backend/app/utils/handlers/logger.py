5e6c4451af19852ff1f0b9c71ac17d8a2e5b7634
darklorian <lolpokens@mail.ru>
2024-03-11 17:23:16 +0400

    create timely buffer handler

--- containers/backend/app/utils/handlers/logger.py
+++ containers/backend/app/utils/handlers/logger.py
@@ -2,5 +2,26 @@ from logging import Handler, LogRecord
 
 
 class SaveHandler(Handler):
-    def emit(self, record: LogRecord):
-        print(record.msg, "LOG")
+    def __init__(self, capacity: int = 256):
+        super().__init__()
+        self.capacity = capacity
+        self.buffer = []
+
+    def save(self):
+        """placeholder to save the buffer to the database"""
+        ...
+
+    def flush(self):
+        """save the buffer and flush it"""
+        self.acquire()
+        try:
+            self.save()
+            self.buffer.clear()
+        finally:
+            self.release()
+
+    def emit(self, record):
+        """Add information to the buffer and flush it if it has more than <capacity> records"""
+        self.buffer.append(record)
+        if len(self.buffer) >= self.capacity:
+            self.flush()
