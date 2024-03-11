6c8046b179155bdabd5b462a335c0d2ca644d82a
darklorian <lolpokens@mail.ru>
2024-03-11 15:39:56 +0400

    add to-do, connect middleware to app

--- containers/backend/app/main.py
+++ containers/backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, APIRouter
+from starlette.middleware import Middleware
 from starlette.requests import Request
+
+from utils.middlewares.logger import LoggerMiddleware
+
+"""
+To-Do:
+add dependency function to set user scope
+async def auth(request: Request):
+    request.scope["user"] = "set in depend"
+
+in app add:
+    Depends(auth)
+"""
+
 
 app = FastAPI(
     title="VixenCarBridge",
+    middleware=[Middleware(LoggerMiddleware)]
 )
 
 main_router = APIRouter(prefix="/api/v1", tags=["Vixen CarBridge"])
@@ -10,7 +25,7 @@ main_router = APIRouter(prefix="/api/v1", tags=["Vixen CarBridge"])
 
 @main_router.get("/status")
 async def pong(request: Request):
-    print(request.headers)
+    print(request.scope)
     return "ok"
 
 app.include_router(main_router)
