b95e0adffa6fb7984fae21860521fdd8a542dbb8
darklorian <lolpokens@mail.ru>
2024-03-14 00:17:51 +0400

    move handler only to access logger

--- containers/backend/app/config.py
+++ containers/backend/app/config.py
@@ -2,7 +2,8 @@ import logging
 
 from utils.logger.handler import SaveHandler
 
-logging.basicConfig(level=logging.DEBUG, handlers=[SaveHandler()])
+logging.basicConfig(level=logging.DEBUG)
 
 db_logger = logging.getLogger('database')  # logger for database access
 access_logger = logging.getLogger('access')  # logger for user access
+access_logger.addHandler(SaveHandler())
