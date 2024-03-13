1f2ee3a696d64704fbda0b2c27d2b8a7c68c0b15
darklorian <lolpokens@mail.ru>
2024-03-13 21:25:57 +0400

    fix typing and add application name to postgres connection

--- containers/backend/app/storages/database/database.py
+++ containers/backend/app/storages/database/database.py
@@ -1,7 +1,9 @@
 from os import getenv
+from typing import AsyncGenerator
 
 from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine
+from sqlalchemy.util import immutabledict
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 url = URL(
@@ -11,13 +13,13 @@ url = URL(
     host=getenv("POSTGRES_HOST"),
     database=getenv("POSTGRES_NAME"),
     port=5432,
-    query={}
+    query=immutabledict({"application_name": "VixenCarBridge"})
 )
 
 engine: AsyncEngine = create_async_engine(url, echo=True, future=True)
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with session_maker() as session:
         yield session
