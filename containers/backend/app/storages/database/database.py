9526bec24a0872be281f06f2b4b8e01e9ce15015
darklorian <lolpokens@mail.ru>
2024-03-14 12:16:40 +0400

    create sync engine, from soft-use in logging handler

--- containers/backend/app/storages/database/database.py
+++ containers/backend/app/storages/database/database.py
@@ -1,25 +1,49 @@
+from enum import Enum
 from os import getenv
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Generator
 
-from sqlalchemy import URL
+from sqlalchemy import URL, create_engine, Engine
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine
-from sqlalchemy.util import immutabledict
+from sqlalchemy.orm import sessionmaker, Session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+
+class Drivers(Enum):
+    async_driver = "asyncpg"
+    driver = "psycopg2"
+
+
 url = URL(
-    drivername="postgresql+asyncpg",
+    drivername=f"postgresql+{Drivers.async_driver.value}",
     username=getenv("POSTGRES_USER"),
     password=getenv("POSTGRES_PASSWORD"),
     host=getenv("POSTGRES_HOST"),
     database=getenv("POSTGRES_NAME"),
     port=5432,
-    query=immutabledict({"application_name": "VixenCarBridge"})
+    query={}
 )
 
-engine: AsyncEngine = create_async_engine(url, echo=True, future=True)
-session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+sync_url = URL(
+    drivername=f"postgresql+{Drivers.driver.value}",
+    username=getenv("POSTGRES_USER"),
+    password=getenv("POSTGRES_PASSWORD"),
+    host=getenv("POSTGRES_HOST"),
+    database=getenv("POSTGRES_NAME"),
+    port=5432,
+    query={}
+)
+engine: AsyncEngine = create_async_engine(url, echo=True)
+asyncsession_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+sync_engine: Engine = create_engine(sync_url, echo=True)
+syncsession_maker = sessionmaker(bind=sync_engine, expire_on_commit=True, class_=Session)
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with session_maker() as session:
+    async with asyncsession_maker() as session:
+        yield session
+
+
+def get_session_sync() -> Generator[Session, None, None]:
+    with syncsession_maker() as session:
         yield session
