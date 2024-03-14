5f3aee171778192cb8e45220f91de67ea6f2a215
darklorian <lolpokens@mail.ru>
2024-03-15 00:32:05 +0400

    fix model dep exception (NF)

--- containers/backend/app/storages/database/models/log_record/model.py
+++ containers/backend/app/storages/database/models/log_record/model.py
@@ -5,10 +5,10 @@ from sqlmodel import Field
 from storages.database.models.base import Base
 
 
-class AuditRecord(Base):
+class AuditRecord(Base, table=True):
     status_code: int
     ip_address: str
-    user_id: uuid.UUID | None = Field(foreign_key="Account.id", nullable=True)
+    user_id: uuid.UUID | None = Field(foreign_key="account.id", nullable=True)
     action: str | None
     endpoint: str
     handle_time: float
