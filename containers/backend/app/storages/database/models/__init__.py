f239563eaa1b325ddb2ec4eb5e37ca864e576198
darklorian <lolpokens@mail.ru>
2024-03-18 21:14:05 +0400

    Add model to alembic storag

---
+++ containers/backend/app/storages/database/models/__init__.py
@@ -0,0 +1,3 @@
+from storages.database.models.log_record.model import AuditRecord
+from storages.database.models.account.model import Account
+from storages.database.models.passport.model import Passport
