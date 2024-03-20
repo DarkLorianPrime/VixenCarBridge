8a47973c4c47c5fa75c5dc6b85167f183e562213
darklorian <lolpokens@mail.ru>
2024-03-20 20:38:38 +0400

    create directory "based" and move models bases to him

--- containers/backend/app/storages/database/models/base/base.py
+++ containers/backend/app/storages/database/models/base/base.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 
-class Base(SQLModel):
+class Base[B](SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, unique=True, nullable=False, index=True)
     created_at: datetime = Field(default_factory=datetime.now)
 
 
-class DeletingBase(Base):
+class DeletingBase[DB](Base):
     removed: bool = Field(default=False)
     removed_at: datetime = Field(default=None)
