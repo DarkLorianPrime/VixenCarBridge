446b61d2603eeb5ef567c12fe61037cc3db36e4b
darklorian <lolpokens@mail.ru>
2024-03-20 20:49:42 +0400

    Applying the new base repo

--- containers/backend/app/storages/database/models/passport/repository.py
+++ containers/backend/app/storages/database/models/passport/repository.py
@@ -1,51 +1,13 @@
-from datetime import datetime
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from storages.database.database import get_session
+from storages.database.models.base.repository import BaseRepository
 from storages.database.models.passport.model import Passport
-from storages.database.models.passport.pydantic_model import PassportModel
 
 
-class PassportRepository:
-    def __init__(
-            self,
-            session: Annotated[AsyncSession, Depends(get_session)]
-    ):
-        self.session = session
-
-    async def create(
-            self,
-            payload: PassportModel
-    ):
-        data = payload.dict()
-        returning = data.pop("returning")
-        stmt = (
-            insert(Passport)
-            .values(**data)
-        )
-        
-        if returning:
-            stmt = stmt.returning(returning)
-        
-        result = await self.session.execute(stmt)
-        await self.session.commit()
-        
-        if returning:
-            scalars = result.scalars()
-            return scalars.first()
-        
-        return result
-
-    def get(self):
-        """to-do:
-            maybe not needed
-        """ 
-        ...
-    
-    def delete(self, instance: Passport):
-        instance.removed = True
-        instance.removed_at = datetime.now()
+class PassportRepository(BaseRepository):
+    def __init__(self, session: Annotated[AsyncSession, Depends(get_session)]):
+        super().__init__(session, Passport)
