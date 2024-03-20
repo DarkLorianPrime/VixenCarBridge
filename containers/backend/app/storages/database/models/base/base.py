4d23ac72133c6d6e654aad8c9b0c6ce2021428a9
darklorian <lolpokens@mail.ru>
2024-03-20 20:50:27 +0400

    Delete test generics

--- containers/backend/app/storages/database/models/base/base.py
+++ containers/backend/app/storages/database/models/base/base.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 
-class Base[B](SQLModel):
+class Base(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True, unique=True, nullable=False, index=True)
     created_at: datetime = Field(default_factory=datetime.now)
 
 
-class DeletingBase[DB](Base):
+class DeletingBase(Base):
     removed: bool = Field(default=False)
     removed_at: datetime = Field(default=None)
