eb9f4a338ff5918c2cb8b34c08441b2898c4ac71
darklorian <lolpokens@mail.ru>
2024-03-23 11:04:18 +0400

    change creation url to url from settings

--- containers/backend/app/storages/database/sync_database.py
+++ containers/backend/app/storages/database/sync_database.py
@@ -1,22 +1,11 @@
-from os import getenv
 from typing import Generator
 
-from sqlalchemy import Engine, create_engine, URL
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from storages.database.enums import Drivers
+from config.settings import settings
 
-sync_url = URL(
-    drivername=f"postgresql+{Drivers.driver.value}",
-    username=getenv("POSTGRES_USER"),
-    password=getenv("POSTGRES_PASSWORD"),
-    host=getenv("POSTGRES_HOST"),
-    database=getenv("POSTGRES_NAME"),
-    port=5432,
-    query={}
-)
-
-sync_engine: Engine = create_engine(sync_url, echo=True)
+sync_engine: Engine = create_engine(settings.get_database_url(is_sync=True), echo=settings.DEBUG)
 syncsession_maker = sessionmaker(bind=sync_engine, expire_on_commit=True, class_=Session)
 
 
