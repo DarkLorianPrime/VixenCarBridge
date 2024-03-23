cd42368c2d9794bb612d4a0d413255ce5ef95715
darklorian <lolpokens@mail.ru>
2024-03-23 11:03:41 +0400

    temporarily move logging config file to _

--- containers/backend/app/_config.py
+++ containers/backend/app/_config.py
@@ -1,8 +1,9 @@
 import logging
 
+from config.settings import settings
 from utils.logger.handler import SaveHandler
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=settings.LOG_LEVEL)
 
 db_logger = logging.getLogger('database')  # logger for database access
 access_logger = logging.getLogger('access')  # logger for user access
